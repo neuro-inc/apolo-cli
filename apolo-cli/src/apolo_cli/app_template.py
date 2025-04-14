5bd89f106dd9545b68b713e63aa3f0dea55f0ca0
Paul Klym <paul.klymenko@apolo.us>
2025-04-14 11:13:00 +0200

    Add app-template ls-versions command (#3228)
    Co-authored-by: Nameless <reacsdas@gmail.com>

--- apolo-cli/src/apolo_cli/app_template.py
+++ apolo-cli/src/apolo_cli/app_template.py
@@ -7,7 +7,7 @@ from .formatters.app_templates import (
     SimpleAppTemplatesFormatter,
 )
 from .root import Root
-from .utils import command, group, option
+from .utils import alias, argument, command, group, option
 
 
 @group()
@@ -64,4 +64,59 @@ async def ls(
                 root.print("No app templates found.")
 
 
+@command()
+@argument("name")
+@option(
+    "--cluster",
+    type=CLUSTER,
+    help="Look on a specified cluster (the current cluster by default).",
+)
+@option(
+    "--org",
+    type=ORG,
+    help="Look on a specified org (the current org by default).",
+)
+@option(
+    "--project",
+    type=PROJECT,
+    help="Look on a specified project (the current project by default).",
+)
+async def ls_versions(
+    root: Root,
+    name: str,
+    cluster: Optional[str],
+    org: Optional[str],
+    project: Optional[str],
+) -> None:
+    """
+    List app template versions.
+    """
+    if root.quiet:
+        templates_fmtr: BaseAppTemplatesFormatter = SimpleAppTemplatesFormatter()
+    else:
+        templates_fmtr = AppTemplatesFormatter()
+
+    templates = []
+    with root.status(f"Fetching versions for app template '{name}'") as status:
+        async with root.client.apps.list_template_versions(
+            name=name, cluster_name=cluster, org_name=org, project_name=project
+        ) as it:
+            async for template in it:
+                templates.append(template)
+                status.update(f"Fetching versions ({len(templates)} loaded)")
+
+    with root.pager():
+        if templates:
+            root.print(templates_fmtr(templates))
+        else:
+            if not root.quiet:
+                root.print(f"No versions found for app template '{name}'")
+
+
+# Register commands with the app_template group
 app_template.add_command(ls)
+app_template.add_command(alias(ls, "list", help=ls.help, deprecated=False))
+app_template.add_command(ls_versions)
+app_template.add_command(
+    alias(ls_versions, "list-versions", help=ls_versions.help, deprecated=False)
+)
