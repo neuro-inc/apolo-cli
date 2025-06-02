f9fc43427add0dad34bae320b36335d331e77236
Dmytro Smyk <porovozls@gmail.com>
2025-06-02 11:58:35 +0200

    extend job description with the namespace (#3264)

--- apolo-sdk/src/apolo_sdk/_jobs.py
+++ apolo-sdk/src/apolo_sdk/_jobs.py
@@ -230,6 +230,7 @@ class JobDescription:
     cluster_name: str
     org_name: Optional[str]
     project_name: str
+    namespace: str
     status: JobStatus
     history: JobStatusHistory
     container: Container
@@ -1091,6 +1092,7 @@ def _job_description_from_api(res: Dict[str, Any], parse: Parser) -> JobDescript
         priority=priority,
         energy_schedule_name=res.get("energy_schedule_name"),
         project_name=res.get("project_name", owner),
+        namespace=res.get("namespace", "") or "",
         _internal=JobDescriptionInternal(
             materialized=res.get("materialized", False),
             being_dropped=res.get("being_dropped", False),
