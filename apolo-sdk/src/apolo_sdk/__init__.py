089c70288a71f242863cbc59b0c9d3c708b011b6
costefan <konstantine.ovchynnikov@apolo.us>
2025-12-04 11:42:17 +0000

    Release 25.12.0

--- apolo-sdk/src/apolo_sdk/__init__.py
+++ apolo-sdk/src/apolo_sdk/__init__.py
@@ -137,7 +137,7 @@ from ._url_utils import CLUSTER_SCHEMES as SCHEMES
 from ._users import Action, Permission, Quota, Share, Users
 from ._utils import _ContextManager, find_project_root
 
-__version__ = "25.11.2"
+__version__ = "25.12.0"
 
 
 __all__ = (
