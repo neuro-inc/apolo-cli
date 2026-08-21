16ce27f06bfe30154eea39c2ba94446cb4aff002
Iaroslav Pyrogov <78590208+yarka-guru@users.noreply.github.com>
2026-08-21 12:00:40 +0200

    Release 26.8.1

--- CHANGELOG.D/3522.feature
+++
@@ -1,1 +0,0 @@
-Applying a config written for another version of the same app with `apolo app configure` no longer fails on a version mismatch. Fields the installed version does not have are named instead.
--- apolo-sdk/src/apolo_sdk/__init__.py
+++ apolo-sdk/src/apolo_sdk/__init__.py
@@ -139,7 +139,7 @@ from ._users import Action, Permission, Quota, Share, Users
 from ._utils import _ContextManager, find_project_root
 from ._vcluster import KubeServiceAccount, VCluster
 
-__version__ = "26.8.0"
+__version__ = "26.8.1"
 
 
 __all__ = (
